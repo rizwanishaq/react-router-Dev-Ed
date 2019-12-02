0db02b71bcdaa994e4136604828199e9ffe366d3
rizwanishaq <rizwanishaq@gmail.com>
2019-12-02 17:46:43 +0100

    Updates

---
+++ precache-manifest.94e6bb28c4c9231d8ed9e3360f3cbf01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27cf0e752948385f46bf691d1c16949b",
+    "url": "/react-router-Dev-Ed/index.html"
+  },
+  {
+    "revision": "242c46b6ae630902aa6c",
+    "url": "/react-router-Dev-Ed/static/css/main.cbb231d0.chunk.css"
+  },
+  {
+    "revision": "2dd375228182f227b53a",
+    "url": "/react-router-Dev-Ed/static/js/2.cb644cee.chunk.js"
+  },
+  {
+    "revision": "242c46b6ae630902aa6c",
+    "url": "/react-router-Dev-Ed/static/js/main.778db012.chunk.js"
+  },
+  {
+    "revision": "87ba4d7b590a916276db",
+    "url": "/react-router-Dev-Ed/static/js/runtime-main.5e6bea2a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-router-Dev-Ed/precache-manifest.94e6bb28c4c9231d8ed9e3360f3cbf01.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-router-Dev-Ed/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
